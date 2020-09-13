22af9fd375b5af82ef44f813aa95f6c328a8ed2a
angelrosmend <angelrosmend@gmail.com>
2020-09-13 18:29:41 -0300

    finished useState lessons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 import './App.css';
+import HookCounterFour from './hooks/useSate/array/HookCounterFour';
 import ClassCounter from './hooks/useSate/counter/ClassCounter';
 import HooksCounter from './hooks/useSate/counter/HooksCounter';
+import HookCounterThree from './hooks/useSate/object/HookCounterThree';
+import HookCounterTwo from './hooks/useSate/prevState/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
       <h1>Import a hook to begin testing</h1>
      {/* <ClassCounter/>
-      <HooksCounter/>*/}
+      <HooksCounter/>
+     <HookCounterTwo/>
+      <HookCounterThree/>
+     */}
+     <HookCounterFour/>
       
     </div>
   );
